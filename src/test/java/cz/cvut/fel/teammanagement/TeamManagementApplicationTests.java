0a7ecd27e63672f444a6b0e1cf5c34c3f61e8132
Patrik Pašek <pasekpat@student.cvut.cz>
2025-10-26 16:18:21 +0100

    [Setup] Spring boot base

---
+++ src/test/java/cz/cvut/fel/teammanagement/TeamManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.teammanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
